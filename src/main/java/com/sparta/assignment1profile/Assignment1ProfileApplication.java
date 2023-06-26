d3a3e4921f9c5570f0d75bbc71cf8d4c16b28417
Kkangjn <jjun1401@naver.com>
2023-06-26 16:51:25 +0900

    git init

---
+++ src/main/java/com/sparta/assignment1profile/Assignment1ProfileApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.assignment1profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1ProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1ProfileApplication.class, args);
+	}
+
+}
